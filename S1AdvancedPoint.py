b755e3322589f92cc050c767ba2405a17b0dbfbb
annasofiaswidorowiczlara <annasofiaswidorowicz@gmail.com>
2026-03-20 11:36:50 +0100

    changed project name

---
+++ S1AdvancedPoint.py
@@ -0,0 +1,47 @@
+from S1ColorPoint import ColorPoint
+
+class AdvancedPoint(ColorPoint):
+    #new class variables
+    COLORS = ["red", "green", "blue", "yellow", "black", "white"]
+    def __init__(self, x, y, color):
+        #check the color
+        if color not in self.COLORS:
+            raise ValueError(f"Invalid color: need to be one of {self.COLORS}")
+        self._x = x
+        self._y = y
+        self._color = color #_ makes it hidden (internal)
+
+    @property #used to access the value now that it's hidden
+    def x(self):
+        return self._x
+    @property
+    def y(self):
+        return self._y
+    @property
+    def color(self):
+        return self._color
+    @color.setter
+    def color(self, color):
+        if value not in self.COLORS:
+            raise ValueError(f"Invalid color: need to be one of {self.COLORS}")
+        self._color = value
+    @classmethod
+    def add_color(cls, color):
+        cls.COLORS.append(color) #add new color to the list
+
+
+p1 = AdvancedPoint(1, 2, "red")
+print(p1)
+p2 = AdvancedPoint(3, 4, "white")
+print(p2)
+#p1.x = 7
+#print(p1.x)
+#call class methods via class name instead of the instance name
+AdvancedPoint.add_color("coral")
+p3 = AdvancedPoint(1, 2, "coral")
+print(p3)
+
+
+
+
+
